380fa5980e6f50c07c46cb85188ce4d9db007380
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-01 15:35:02 -0400

    some more setup

--- kafka.sh
+++ kafka.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname "$0")
+mkdir -p ${CURRENT_DIR}/data/kafka
 kafka-server-start ${CURRENT_DIR}/server.properties
--- zookeeper.sh
+++ zookeeper.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CURRENT_DIR=$(dirname "$0")
+mkdir -p ${CURRENT_DIR}/data/zookeeper
 zookeeper-server-start ${CURRENT_DIR}/zookeeper.properties
