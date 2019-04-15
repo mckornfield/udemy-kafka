7431d741daab6d7ff2554fc33bed08146ce3ad96
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-14 20:59:51 -0400

    added for loop

--- kafka-simple/src/main/java/com/github/mckornfield/tutorial1/ProducerDemo.java
+++ kafka-simple/src/main/java/com/github/mckornfield/tutorial1/ProducerDemo.java
@@ -1,13 +1,19 @@
 package com.github.mckornfield.tutorial1;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ProducerDemo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerDemo.class);
 
     public static void main(String[] args) {
         Properties properties = new Properties();
@@ -17,12 +23,27 @@ public class ProducerDemo {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("first-topic","Hello World!");
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first-topic", "Hello World #" + i);
 
-        producer.send(producerRecord);
+            // Sends data asynchronously
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception == null) {
+                        LOGGER.info("Received new metadata. Topic: {}, Partition: {}, Offset: {}, Timestamp: {}",
+                            metadata.topic(), metadata.partition(), metadata.offset(), metadata.timestamp());
 
+                    } else {
+                        LOGGER.info("Error while producing", exception);
+                    }
+                }
+            });
+        }
+
+        // Flush asynchronous data
         producer.flush();
         producer.close();
     }
