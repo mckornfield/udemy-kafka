e910fef45c7e05be45d61d0902f44717c3a44b25
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-04-15 07:04:37 -0400

    added keys and removed .idea from source control

--- kafka-simple/src/main/java/com/github/mckornfield/tutorial1/ProducerDemo.java
+++ kafka-simple/src/main/java/com/github/mckornfield/tutorial1/ProducerDemo.java
@@ -1,21 +1,20 @@
 package com.github.mckornfield.tutorial1;
 
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ProducerDemo {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProducerDemo.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties properties = new Properties();
         String bootstrapServers = "127.0.0.1:9092";
 
@@ -26,7 +25,16 @@ public class ProducerDemo {
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         for (int i = 0; i < 10; i++) {
-            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "Hello World #" + i);
+            String firstTopic = "first_topic";
+            String key = "id_" + i;
+            String value = "Hello World #" + i;
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(firstTopic, key, value);
+
+            LOGGER.info("Key is {}", key);
+            // id0 is partition 1
+            // id1 is partition 0
+            // id2 is partition 2
+            // etc.
 
             // Sends data asynchronously
             producer.send(producerRecord, (metadata, exception) -> {
@@ -37,7 +45,7 @@ public class ProducerDemo {
                 } else {
                     LOGGER.info("Error while producing", exception);
                 }
-            });
+            }).get(); // Make it synchronous, don't do in production
         }
 
         // Flush asynchronous data
